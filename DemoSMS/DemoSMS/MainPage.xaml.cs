0ff2a3fcc6a93a9f0ec81bc9f3f6dbf37e42fa7f
Juliano Custódio <juliano.custodio@hotmail.com.br>
2018-05-18 12:22:47 -0300

    Implement Send SMS

--- DemoSMS/DemoSMS/MainPage.xaml.cs
+++ DemoSMS/DemoSMS/MainPage.xaml.cs
@@ -1,17 +1,21 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Plugin.Messaging;
 using Xamarin.Forms;
 
 namespace DemoSMS
 {
-	public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void Send(object sender, EventArgs e)
+        {
+            var smsMessenger = CrossMessaging.Current.SmsMessenger;
+            if (smsMessenger.CanSendSms)
+                smsMessenger.SendSms(Number.Text, Message.Text);
+        }
+    }
 }
